21f04e71a03da0c77d90fe62c7538207d0ed42ce
manofsteal <nguyendinhdung8695@gmail.com>
2016-08-27 17:28:57 +0700

    reorgnize the folder structer

---
+++ Server/Gernaratekey/test.js
@@ -0,0 +1,31 @@
+
+
+// Nodejs encryption with CTR
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd6F3Efeq';
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+ 
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+ 
+var hw = encrypt("tenant3")
+// outputs hello world
+console.log(hw);
+console.log(decrypt(hw));
+
+/*
+// cau hinh Tenant cho device, điền id thay vì tenant
+ 108bda912f6529 tenant1 
+ 108bda912f652a tenant2
+ */
